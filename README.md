d81453560d4bb4099d015210ab07ae22e6b5562e
haibo.xiong <haibo.xiong@ly.com>
2018-06-04 18:24:54 +0800

    依赖maven本地库的公共项目

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # futureOne
 打算写点干货
+1、遇到一个问题：
+请求：http://localhost:8080/hello其中参数为{"name":"19508","password":"123123"}的时候：
+返回：Springmvc rest 传递json报415错误:The server refused this request because the request entity is in a format
+解决方案：添加jar包依赖：
+    <dependency>
+      <groupId>com.fasterxml.jackson.core</groupId>
+      <artifactId>jackson-databind</artifactId>
+      <version>${jackson.version}</version>
+    </dependency>
+    <dependency>
+      <groupId>com.fasterxml.jackson.core</groupId>
+      <artifactId>jackson-core</artifactId>
+      <version>${jackson.version}</version>
+    </dependency>
+    <dependency>
+      <groupId>com.fasterxml.jackson.core</groupId>
+      <artifactId>jackson-annotations</artifactId>
+      <version>${jackson.version}</version>
+    </dependency>
+
+2、公共项目commonBean的使用    
--- src/main/java/com/haibo/future/web/ctrl/HelloMvc.java
+++ src/main/java/com/haibo/future/web/ctrl/HelloMvc.java
@@ -1,14 +1,20 @@
 package com.haibo.future.web.ctrl;
 
+import com.haibo.common.request.UserRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloMvc {
-    @RequestMapping(value = "/hello", produces = "text/html;charset=UTF-8")
+
+    @RequestMapping(value = "/hello", method = RequestMethod.POST)
     @ResponseBody
-    public String hello() {
-        return "你好";
+    public String hello(@RequestBody UserRequest request) {
+        String name = request.getName();
+        String password = request.getPassword();
+        return "hello:"+name+":"+password;
     }
 }
