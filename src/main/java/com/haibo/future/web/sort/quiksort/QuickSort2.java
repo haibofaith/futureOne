bdd940957a46c7a6703d3f63a35867b86ea8c130
haibo.xiong <haibo.xiong@ly.com>
2018-08-27 22:09:54 +0800

    面试题：快速排序的写法

---
+++ src/main/java/com/haibo/future/web/sort/quiksort/QuickSort2.java
@@ -0,0 +1,51 @@
+package com.haibo.future.web.sort.quiksort;
+
+//自己写的，更具aha算法
+public class QuickSort2 {
+    public static int[] quickSort2(int[] arrays, int low, int high) {
+        if (low>high){
+            return arrays;
+        }
+        //1、左边第一个开始
+        int temp = arrays[low];
+        int left = low;
+        int right = high;
+        //2.1、从右到左，找到第一个比temp小的数的位置
+        //2.2、从左到右，找到第一个比temp大的数的位置
+        //2.3、交换位置
+        while (left!=right){
+            while (temp<=arrays[right]&&left<right){
+                right--;
+            }
+            while (temp>=arrays[left]&&left<right){
+                left++;
+            }
+            if (left<right){
+                swap(arrays,left,right);
+                log(arrays);
+            }
+        }
+        //第一个和当前位置的交换
+        arrays[low] = arrays[left];
+        arrays[left] = temp;
+        log(arrays);
+        //左侧
+        quickSort2(arrays,low,left-1);
+        //右侧
+        quickSort2(arrays,left+1,high);
+        return arrays;
+    }
+
+    private static void swap(int[] arrays, int left, int right){
+        int temp = arrays[left];
+        arrays[left] = arrays[right];
+        arrays[right]=temp;
+    }
+
+    private static void log(int[] arrays){
+        System.out.println("");
+        for (int ay :arrays){
+            System.out.print(" "+ay);
+        }
+    }
+}
