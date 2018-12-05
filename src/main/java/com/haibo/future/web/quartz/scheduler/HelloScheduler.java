fcef0d4a0e6edc2a24fa1a441bf6c2e807839b8c
haibo.xiong <haibo.xiong@ly.com>
2018-12-05 15:59:55 +0800

    Log4j缺省日志配置

--- src/main/java/com/haibo/future/web/quartz/scheduler/HelloScheduler.java
+++ src/main/java/com/haibo/future/web/quartz/scheduler/HelloScheduler.java
@@ -1,8 +1,10 @@
 package com.haibo.future.web.quartz.scheduler;
 
 import com.haibo.future.web.quartz.jobs.HelloJob;
+import org.apache.log4j.spi.LoggerFactory;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,10 +15,15 @@ import java.util.Date;
  * @description:
  */
 public class HelloScheduler {
+    public static Logger logger = org.slf4j.LoggerFactory.getLogger(HelloScheduler.class);
     public static void main(String[] args) throws SchedulerException {
         //1、创建JobDetail
         JobDetail jobDetail = JobBuilder.newJob(HelloJob.class)
                 .withIdentity("myJob","group1").build();
+//        logger.info("jobDetail name:"+jobDetail.getKey().getName());
+        System.out.println("jobDetail name:"+jobDetail.getKey().getName());
+        System.out.println("jobDetail group:"+jobDetail.getKey().getGroup());
+        System.out.println("jobDetail class:"+jobDetail.getJobClass().getName());
         //2、创建Trigger
         Trigger trigger = TriggerBuilder.newTrigger()
                 .withIdentity("myTrigger","triGroup1").startNow()
